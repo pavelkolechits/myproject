19996800e8987236f2197a0047497a14555bebde
pavelkolechits <75511210+pavelkolechits@users.noreply.github.com>
2021-07-14 20:59:10 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
+import "./App.css";
 
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-import { useSelector,useDispatch } from 'react-redux';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-
-
-
-import {ACTIONS} from "./redux/constants"
-import {RouteComponent} from "./components/RouteComponent/RouteComponent"
-
-
+import { ACTIONS } from "./redux/constants";
+import { RouteComponent } from "./components/RouteComponent/RouteComponent";
 
 function App() {
-return(
-	<Provider store={store}>
-	<RouteComponent/>
-	</Provider>
-)
-
+  return (
+    <Provider store={store}>
+      <RouteComponent />
+    </Provider>
+  );
 }
 export default App;
